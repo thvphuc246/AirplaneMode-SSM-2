99cecc216ca0126c5aa5af2dc489ad60351eae56
Thong <khanhly2995@gmail.com>
2018-05-03 15:04:23 +0300

    Minor changes add-new-log form; added add-new-task form

--- ui/tasks.js
+++ ui/tasks.js
@@ -1,1 +1,10 @@
-//NOTHING YET
+function addButton() {
+    document.getElementById("hidden-add-form").style.visibility = "visible";
+}; //The plus sign button that shows the form
+
+function cancelButton() {
+    document.getElementById("hidden-add-form").style.visibility = "hidden";
+    document.getElementById("my-form").reset();
+
+    /* document.getElementById('urgent-btn').checked = false; */
+}; // The X sign button that cancel add task in the form
